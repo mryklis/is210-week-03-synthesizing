337d167dbcc3b914e2ac59ccbec15194f30a3d9d
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-10 17:09:53 +0000

    changes to this repo

--- task_02.py
+++ task_02.py
@@ -2,5 +2,17 @@
 # -*- coding: utf-8 -*-
 """Contains expectations."""
 
-
 import inquisition
+
+FLEMISH = inquisition.SPANISH
+
+word = 'Spanish'
+length= len(word)
+found=inquisition.SPANISH.index('Spanish')
+
+part1 = FLEMISH[:found]
+part2 = FLEMISH[(found + length):]
+
+FLEMISH = part1 + 'Flemish' + part2
+
+print FLEMISH
